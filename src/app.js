0b206c1acb9d075c03b073d8329378b10040bb7d
Calmes <christophe.calmes@laplateforme.io>
2023-12-19 14:03:15 +0100

    first road post

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
 const app = express();
+// Middleware permet de lire du JSON.
+app.use(express.json());
 
 const movieControllers = require("./controllers/movieControllers");
 const usersControllers = require("./controllers/usersControllers");
@@ -9,5 +11,7 @@ app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.get("/api/users", usersControllers.getAllUsers);
 app.get("/api/users/:id", usersControllers.getOneUser);
+
+app.post("/api/movies", movieControllers.postMovie);
 
 module.exports = app;
