eba75a0b1a5b80a4a44a1d40648ca2afb3744f97
Calmes <christophe.calmes@laplateforme.io>
2023-12-20 10:03:25 +0100

    bug put et post

--- src/app.js
+++ src/app.js
@@ -6,14 +6,13 @@ app.use(express.json());
 
 const movieControllers = require("./controllers/movieControllers");
 const usersControllers = require("./controllers/usersControllers");
-const validateMovie = require("./middlewares/validateMovie");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.get("/api/users", usersControllers.getAllUsers);
 app.get("/api/users/:id", usersControllers.getOneUser);
 
-app.post("/api/movies", validateMovie, movieControllers.postMovie);
+app.post("/api/movies", movieControllers.postMovie);
 app.post("/api/users", usersControllers.postNewUser);
 
 app.put("/api/movies/:id", movieControllers.updateMovie);
